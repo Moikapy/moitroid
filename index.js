e790f5bac281d1456a01e51cb45c510575d70cf9
Warren Gates <warrenckhan@gmail.com>
2019-05-25 03:17:52 -0400

    initial commit for astroid game

---
+++ index.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+dotenv.config();
+const port = process.env.PORT || 8081;
+app.use(cors());
+
+	app.get('/', function(req, res) {
+		res.sendFile(__dirname + '/public/index.html');
+	});
+app.use('/', express.static('.'));
+// Sets what Port the Server is listening on
+app.listen(port, () => {
+	console.log(`Listening to http://localhost:${port}`);
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+io.on('disconnected', function(socket) {
+	console.log('a user disconnected');
+});
