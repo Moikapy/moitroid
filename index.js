aab63eb364f7c106c24f1bf247237905a7d793a5
Warren Gates <warrenckhan@gmail.com>
2019-05-25 23:53:20 -0400

    fixed styling and added bootstrap[

--- index.js
+++ index.js
@@ -11,17 +11,18 @@ dotenv.config();
 const port = process.env.PORT || 8081;
 app.use(cors());
 
-	app.get('/', function(req, res) {
-		res.sendFile(__dirname + '/public/index.html');
-	});
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+});
 app.use('/', express.static('.'));
+app.use('/', express.static('./public'));
 // Sets what Port the Server is listening on
 app.listen(port, () => {
 	console.log(`Listening to http://localhost:${port}`);
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on('connection', function(socket) {
+	console.log('a user connected');
 });
 
 io.on('disconnected', function(socket) {
